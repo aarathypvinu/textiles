46b01643426e0ce8023b3ab349a5df069d444a54
aarathypvinu <aarathypvinu@gamil.com>
2021-03-16 19:24:14 +0530

    textile

--- index.php
+++ index.php
@@ -32,9 +32,7 @@
                   <li class="nav-item">
                     <a class="nav-link" href="about.php">about us</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="contact.php">contact us</a>
-                  </li>
+                  
                 </ul>
                 <form class="d-flex">
                   <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
